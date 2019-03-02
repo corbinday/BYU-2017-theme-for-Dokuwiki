44ce85b810b5b6c550d3b798e9dc1e20d5369461
Corbin <raizingthebar@gmail.com>
2019-03-01 17:40:12 -0700

    Rough changes
    Added BYU header and footer, removed dokuwiki header. Added a BYU nav menu. Still some code cleaning to do, and tweaks.

--- main.php
+++ main.php
@@ -23,66 +23,147 @@ $showSidebar = $hasSidebar && ($ACT=='show');
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
     <?php tpl_includeFile('meta.html') ?>
+
+
+    <!--BYU header source -->
+        <script async src="https://cdn.byu.edu/byu-theme-components/1.x.x/byu-theme-components.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.byu.edu/byu-theme-components/1.x.x/byu-theme-components.min.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!--Styling for Sticky BYU Footer-->
+            <style>
+            html, body {
+                height: 100%;
+            }
+
+            .containing-element {
+                display: flex;
+                flex-direction: column;
+                height: 100%;
+            }
+
+            .page-content {
+                flex-grow: 1;
+                align-items: center;
+            }
+            </style>
+
+    <!--End BYU header source-->
+
 </head>
 
 <body>
-    <div id="dokuwiki__site"><div id="dokuwiki__top" class="site <?php echo tpl_classes(); ?> <?php
-        echo ($showSidebar) ? 'showSidebar' : ''; ?> <?php echo ($hasSidebar) ? 'hasSidebar' : ''; ?>">
+    <div class="containing-element">
+        <byu-header full-width>
+            <h1 slot="site-title" >ECEn Support Wiki</h1> 
 
-        <?php include('tpl_header.php') ?>
+            <!--BUTTONS-->
+            <?php
+                /*if (!empty($_SERVER['REMOTE_USER']) && $INFO['isadmin']) {
+                    //Admin tools button
+                    echo "<a slot=\"actions\" href=\"http://127.0.0.1/dokuwiki/doku.php?do=admin&id=user%3Aadmin%3Astart\" target=\"_self\">Admin</a>";
+                }*/
+                if (!empty($_SERVER['REMOTE_USER'])) {
+                    //Logout Button
+                    echo "<a slot=\"actions\" href=\"javascript:{}\" onclick=\"document.getElementById('log_out').submit(); return false;\">Log Out
+                                <form id=\"log_out\" method=\"get\" action=\"/dokuwiki/doku.php\">
+                                    <input type=\"hidden\" name=\"do\" value=\"logout\">
+                                    <input type=\"hidden\" name=\"sectok\" value=\"756fc1becd1e22260cc6a2f74d2e2715\">
+                                    <input type=\"hidden\" name=\"id\" value=\"start\">
+                                </form>
+                        </a>";
+                    }
+                if (empty($_SERVER['REMOTE_USER'])) {
+                    //Login Button
+                    echo "<a slot=\"actions\" href=\"javascript:{}\" onclick=\"document.getElementById('log_in').submit(); return false;\">Log In
+                                <form id=\"log_in\" method=\"get\" action=\"/dokuwiki/doku.php\">
+                                    <input type=\"hidden\" name=\"do\" value=\"login\">
+                                    <input type=\"hidden\" name=\"sectok\" value=\"\">
+                                    <input type=\"hidden\" name=\"id\" value=\"start\">
+                                </form>
+                        </a>";
+                }
+            ?> 
 
-        <div class="wrapper group">
+            <!--font size css seems to be the cause of the disalignment of the search button and search box PLEASE FIX -->
+            <byu-search slot="search" action="navigate" action-target="http://127.0.0.1/dokuwiki/doku.php?do=search&id=start&q=${search}" method="get" placeholder="Search"></byu-search>
 
-            <?php if($showSidebar): ?>
-                <!-- ********** ASIDE ********** -->
-                <div id="dokuwiki__aside"><div class="pad aside include group">
-                    <h3 class="toggle"><?php echo $lang['sidebar'] ?></h3>
-                    <div class="content"><div class="group">
+            <byu-menu slot="nav" collapsed>
+                <a href="/dokuwiki/doku.php?id=start">Home</a>
+                <a href= "/dokuwiki/doku.php?id=start&do=index">Sitemap</a>
+                <a href="/dokuwiki/doku.php?id=start&do=recent">Recent Changes</a>
+                <a href="http://127.0.0.1/dokuwiki/doku.php?id=start&do=media&ns=">Media Manager</a>
+                <?php //Admin tools link
+                    if (!empty($_SERVER['REMOTE_USER']) && $INFO['isadmin']) {
+                        echo "<a href=\"http://127.0.0.1/dokuwiki/doku.php?do=admin&id=user%3Aadmin%3Astart\" target=\"_self\">Admin</a>";
+                    }
+                ?>
+            </byu-menu>
+        </byu-header>
+
+        <div class="page-content">
+                <h1 style="text-align:center;"> HOW CAN WE HELP? </h1>
+            <div id="dokuwiki__site"><div id="dokuwiki__top" class="site <?php echo tpl_classes(); ?> <?php
+                echo ($showSidebar) ? 'showSidebar' : ''; ?> <?php echo ($hasSidebar) ? 'hasSidebar' : ''; ?>">
+
+            <!--REMOVE THIS -->
+            <?php //include('tpl_header.php') ?>
+
+                <div class="wrapper group">
+
+                    <?php if($showSidebar): ?>
+                        <!-- ********** ASIDE ********** -->
+                        <div id="dokuwiki__aside"><div class="pad aside include group">
+                            <h3 class="toggle"><?php echo $lang['sidebar'] ?></h3>
+                            <div class="content"><div class="group">
+                                <?php tpl_flush() ?>
+                                <?php tpl_includeFile('sidebarheader.html') ?>
+                                <?php tpl_include_page($conf['sidebar'], true, true) ?>
+                                <?php tpl_includeFile('sidebarfooter.html') ?>
+                            </div></div>
+                        </div></div><!-- /aside -->
+                    <?php endif; ?>
+
+                    <!-- ********** CONTENT ********** -->
+                    <div id="dokuwiki__content"><div class="pad group">
+                        <?php html_msgarea() ?>
+
+                        <div class="pageId"><span><?php echo hsc($ID) ?></span></div>
+
+                        <div class="page group">
+                            <?php tpl_flush() ?>
+                            <?php tpl_includeFile('pageheader.html') ?>
+                            <!-- wikipage start -->
+                            <?php tpl_content() ?>
+                            <!-- wikipage stop -->
+                            <?php tpl_includeFile('pagefooter.html') ?>
+                        </div>
+
+                        <div class="docInfo"><?php tpl_pageinfo() ?></div>
+
                         <?php tpl_flush() ?>
-                        <?php tpl_includeFile('sidebarheader.html') ?>
-                        <?php tpl_include_page($conf['sidebar'], true, true) ?>
-                        <?php tpl_includeFile('sidebarfooter.html') ?>
-                    </div></div>
-                </div></div><!-- /aside -->
-            <?php endif; ?>
+                    </div></div><!-- /content -->
 
-            <!-- ********** CONTENT ********** -->
-            <div id="dokuwiki__content"><div class="pad group">
-                <?php html_msgarea() ?>
+                    <hr class="a11y" />
 
-                <div class="pageId"><span><?php echo hsc($ID) ?></span></div>
+                    <!-- PAGE ACTIONS -->
+                    <div id="dokuwiki__pagetools">
+                        <h3 class="a11y"><?php echo $lang['page_tools']; ?></h3>
+                        <div class="tools">
+                            <ul>
+                                <?php echo (new \dokuwiki\Menu\PageMenu())->getListItems(); ?>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /wrapper -->
 
-                <div class="page group">
-                    <?php tpl_flush() ?>
-                    <?php tpl_includeFile('pageheader.html') ?>
-                    <!-- wikipage start -->
-                    <?php tpl_content() ?>
-                    <!-- wikipage stop -->
-                    <?php tpl_includeFile('pagefooter.html') ?>
-                </div>
+                <?php //include('tpl_footer.php') ?>
+            </div></div><!-- /site -->
 
-                <div class="docInfo"><?php tpl_pageinfo() ?></div>
-
-                <?php tpl_flush() ?>
-            </div></div><!-- /content -->
-
-            <hr class="a11y" />
-
-            <!-- PAGE ACTIONS -->
-            <div id="dokuwiki__pagetools">
-                <h3 class="a11y"><?php echo $lang['page_tools']; ?></h3>
-                <div class="tools">
-                    <ul>
-                        <?php echo (new \dokuwiki\Menu\PageMenu())->getListItems(); ?>
-                    </ul>
-                </div>
-            </div>
-        </div><!-- /wrapper -->
-
-        <?php include('tpl_footer.php') ?>
-    </div></div><!-- /site -->
-
-    <div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
-    <div id="screen__mode" class="no"></div><?php /* helper to detect CSS media query in script.js */ ?>
+            <div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
+            <div id="screen__mode" class="no"></div><?php /* helper to detect CSS media query in script.js */ ?>
+        </div>
+        <byu-footer></byu-footer>
+    </div>
 </body>
 </html>
